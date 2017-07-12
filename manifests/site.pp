9a4b4b8b9ebcd33eb9aa87088dd05b5180414e6b
CharlesPlachno <cplachno@uoregon.edu>
2017-07-12 16:36:18 -0700

    added cron job to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,9 @@ node ip-10-0-2-190{
 node ip-10-0-2-127{
 }
 node ip-10-0-2-242{
+    cron { "puppet update":
+        command=> "cd /etc/puppet && git pull -q origin master",
+        user   => root,
+        minute => "*/5",
+    }
 }
