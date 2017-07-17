4915e5594daed805060d3ddfb8e46278800d37b0
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-07-17 02:30:11 -0700

    ADDING include sshd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,12 @@
 node ip-10-0-2-190 {
+    include sshd
 }
 node ip-10-0-2-127 {
+    include sshd
 }
 node ip-10-0-2-242 {
+    include sshd
+
     cron { "puppet update":
         command=> "cd /etc/puppet && git pull -q origin master",
         user   => root,
