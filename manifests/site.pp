1588b16e45968a73ffa8f46a70148933ca6e9536
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-07-17 00:55:51 -0700

    adding spaces

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,8 @@
-node ip-10-0-2-190{
+node ip-10-0-2-190 {
 }
-node ip-10-0-2-127{
+node ip-10-0-2-127 {
 }
-node ip-10-0-2-242{
+node ip-10-0-2-242 {
     cron { "puppet update":
         command=> "cd /etc/puppet && git pull -q origin master",
         user   => root,
