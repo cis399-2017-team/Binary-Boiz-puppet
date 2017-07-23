f3f9e693062e3840906b93e804598470fe3f7e07
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-07-23 14:47:31 -0700

    Update init.pp

---
+++ modules/apserver/manifests/init.pp
@@ -0,0 +1,36 @@
+class apserver {
+	
+  package { "apache2": 
+		ensure => installed,
+	}
+  
+	service { "apache2":
+		ensure => running,
+		enable => true,
+		require => [ Package['apache2'], File["/etc/apache2/apache2.conf"] ],
+		subscribe => File["/etc/apache2/apache2.conf"],
+		hasstatus => true,
+		hasrestart => true,
+	}
+  
+  file { '/var/www/html': 
+		ensure => directory,
+		require => Package['apache2'],
+		recurse => true,
+		owner => root,
+		group => root,
+		source => "puppet:///modules/apache/html/",
+		mode => 0644,
+	} 
+
+	file { "apache2.conf":
+		ensure => present,
+		path => "/etc/apache2/apache2.conf",
+ 		source => "puppet:///modules/apache/apache2.conf",
+		mode => 0640,
+		owner => root,
+		group => root,
+		require => Package['apache2'],
+	}
+  
+}
