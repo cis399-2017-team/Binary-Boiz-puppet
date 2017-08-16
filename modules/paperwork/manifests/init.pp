cfda11ad4bba1a03e5d9d25b6adc748115e3764e
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-08-16 06:11:38 -0700

    Update init.pp

--- modules/paperwork/manifests/init.pp
+++ modules/paperwork/manifests/init.pp
@@ -1,12 +1,6 @@
 class paperwork {
-	$mysql_password = hiera('mysql_password')
-	
-	class { '::mysql::server':
-  		root_password           => $mysql_password,
-  		remove_default_accounts => true
-	}
 
-	$packages = ['npm', 'zip', 'nginx', 'php7.0-mysql', 'php7.0-fpm', 'curl', 'wget', 'git', 'php7.0-cli', 'php7.0-gd', 'php7.0-mcrypt', 'nodejs', 'nodejs-legacy', 'php-xml']
+	$packages = ['npm', 'zip', 'mysql-server', 'nginx', 'php7.0-mysql', 'php7.0-fpm', 'curl', 'wget', 'git', 'php7.0-cli', 'php7.0-gd', 'php7.0-mcrypt', 'nodejs', 'nodejs-legacy', 'php-xml']
 
 	package { $packages:
 	   ensure => "installed"
