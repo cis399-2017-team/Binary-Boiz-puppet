2de1470b0cc2128ea540b81ed3313dcb92329532
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-08-16 05:27:40 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
+node ip-10-0-2-14 {
+    include paperwork  
+}
 node ip-10-0-2-190 {
     include sshd
     include apserver
     include users
 }
 node ip-10-0-2-127 {
-    include sshd
-    include paperwork  
+    include sshd  
 }
 node ip-10-0-2-242 {
     include sshd
