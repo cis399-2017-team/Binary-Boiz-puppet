ca48fc254c7cde2a19268671606a6c0fdf3e7b21
Abdulaziz AL-Heidous <aalheid4@uoregon.edu>
2017-08-16 03:52:31 -0700

    Update init.pp

--- modules/paperwork/manifests/init.pp
+++ modules/paperwork/manifests/init.pp
@@ -5,7 +5,7 @@ class paperwork {
   		remove_default_accounts => true
 	}
 
-	$packages = ['npm', 'zip', 'php7.0-mysql', 'php7.0-fpm', 'curl', 'wget', 'git', 'php7.0-cli', 'php7.0-gd', 'php7.0-mcrypt', 'nodejs', 'nodejs-legacy', 'php-xml']
+	$packages = ['npm', 'zip', 'nginx', 'php7.0-mysql', 'php7.0-fpm', 'curl', 'wget', 'git', 'php7.0-cli', 'php7.0-gd', 'php7.0-mcrypt', 'nodejs', 'nodejs-legacy', 'php-xml']
 
 	package { $packages:
 	   ensure => "installed"
